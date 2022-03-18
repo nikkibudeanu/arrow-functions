b52f39b8d3b745c787f9522f4e6e14c2e5f3edd7
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-03-18 15:26:34 +0000

    add single line arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,12 +4,16 @@
  */
 
 // Vanilla JavaScript Function
+
+// Arrow Function With Parameters
+
 function addTwoNumbers(a, b){
     //code block
     return a+b;
 }
 let sum = addTwoNumbers(3,5)
 console.log(sum);
+
 
 // ES6
 const addTwoNumbers2 = (c,d) => {
@@ -18,9 +22,11 @@ const addTwoNumbers2 = (c,d) => {
 let sum2 = addTwoNumbers2(3,5)
 console.log(sum2);
 
-// Arrow Function With Parameters
-
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 =(e, f) => e + f;
+let sum3 = addTwoNumbers3(4, 5);
+console.log(sum3)
+// also valid --> const addTwoNumbers3 =(e, f) =>( e + f);
 
 // Implicit Returns
 
